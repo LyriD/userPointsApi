e12e93f7b227591558fb49755b29b9791bdd5c9c
lyrs <lyrs@me.com>
2014-11-04 20:14:34 +0300

    рабочая ведомость с пользователями и автообновлением

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,9 +6,7 @@ module Api
 
     def change_rate
       if @user.update!(user_params)
-        PrivatePub.publish_to "/change_user_rate/change", :chat_message => "Hello, world!"
-        # PrivatePub.publish_to("/change_user_rate/change")
-
+        PrivatePub.publish_to "/change_user_rate/change", {user_id: @user.id, user_total: @user.total_points}
         render nothing: true #возвращаем 200 статус если все ок
       else
         render json: get_resource.errors, status: :unprocessable_entity
